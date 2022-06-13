5d46fbd85f44c547837bcef97e3c433a8e682d97
SiriusBrightstar <siriusbrightstar@protonmail.com>
2022-06-13 20:44:17 +0530

    Create ergast.py

---
+++ ergast.py
@@ -0,0 +1,20 @@
+# Getting data using Ergast API
+
+import json
+import requests
+from pprint import pprint
+
+from dateutil.tz import gettz
+
+url = requests.get('http://ergast.com/api/f1/current.json')
+jsonData = url.text
+data = json.loads(jsonData)
+pprint(data['MRData']['RaceTable']['Races'])
+
+date = data['MRData']['RaceTable']['Races'][0]['date']
+time = data['MRData']['RaceTable']['Races'][0]['time']
+dtStr = date + ' ' + time
+dtUTC = datetime.strptime(dtStr, '%Y-%m-%d %H:%M:%SZ')
+utc = dtUTC.replace(tzinfo=tz.gettz('UTC'))
+local = utc.astimezone(tz.gettz('Asia/Kolkata'))
+print(local.isoformat(timespec='seconds'))
